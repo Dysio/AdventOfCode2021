16bceaf22cb8ae99f0629760df5f72d114c9f746
TDyszewski <tomasz.dyszewski@dynatrace.com>
2021-12-08 18:21:10 +0100

    day 3 part 2

--- 3_day/3_day.py
+++ 3_day/3_day.py
@@ -1,5 +1,3 @@
-input = 'input.txt'
-
 def bin_to_dec(num: str):
     number = 0
     for elem in range(1, len(num) + 1):
@@ -33,6 +31,58 @@ def gamma_epsilon_rate(data):
 
     return gamma_rate, epsilon_rate, result
 
-print(gamma_epsilon_rate(input))
+
+def oxygen_co2_rating(data, value:str):
+    with open(data) as f:
+        file = f.readlines()
+
+    num_list = file
+
+    if value == 'oxygen':
+        for digit in range(len(file[0].strip())):
+            list_zero = []
+            list_one = []
+            for num in num_list:
+                if num[digit] == "1":
+                    list_one.append(num)
+                elif num[digit] == "0":
+                    list_zero.append(num)
+            if len(list_one) > len(list_zero):
+                num_list = list_one
+            elif len(list_one) < len(list_zero):
+                num_list = list_zero
+            else:
+                num_list = list_one
+            if len(num_list) == 1:
+                break
+    else:
+        for digit in range(len(file[0].strip())):
+            list_zero = []
+            list_one = []
+            for num in num_list:
+                if num[digit] == "1":
+                    list_one.append(num)
+                elif num[digit] == "0":
+                    list_zero.append(num)
+            if len(list_one) < len(list_zero):
+                num_list = list_one
+            elif len(list_one) > len(list_zero):
+                num_list = list_zero
+            else:
+                num_list = list_zero
+            if len(num_list) == 1:
+                break
+
+    return bin_to_dec(num_list[0].strip())
 
 
+if __name__ == '__main__':
+    input = 'input.txt'
+
+    print(gamma_epsilon_rate(input))
+
+    oxygen_generator_rating = oxygen_co2_rating(input, 'oxygen')
+    co2_scrubber_rating = oxygen_co2_rating(input, 'co2')
+    print(f'oxygen generator rating = {oxygen_generator_rating}')
+    print(f'co2 scrubber rating = {co2_scrubber_rating}')
+    print(f'life support rating = {oxygen_generator_rating * co2_scrubber_rating}')
